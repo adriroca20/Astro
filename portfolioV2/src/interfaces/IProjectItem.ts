25cf1047dc7b255ad3ab599e00bfdca2b4890eb6
adriroca20 <hardhe21@gmail.com>
2024-01-19 00:35:25 +0100

    Added footer and main projects section

--- portfolioV2/src/interfaces/IProjectItem.ts
+++ portfolioV2/src/interfaces/IProjectItem.ts
@@ -3,3 +3,16 @@ export interface IProjectItem {
     githubLink: string;
     description: string
 }
+export interface IMainProject{
+    image:string, 
+    title:string, 
+    description:string, 
+    tags:ITags[], 
+    link:string, 
+    github?:string
+}
+interface ITags{
+    name: string,
+    class: string,
+    icon: any,
+}
