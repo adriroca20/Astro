750ded6ba97aeb214069f125bbde1ef6eac2d41e
adriroca20 <hardhe21@gmail.com>
2024-01-23 18:02:33 +0100

    feat: Created recorder option component

--- free-screen-recorder/src/components/RecordSection.svelte
+++ free-screen-recorder/src/components/RecordSection.svelte
@@ -1,9 +1,15 @@
-<section class="flex w-[60%] h-screen justify-center items-center flex-col gap-5">
+<script>
+    import RecorderOption from "./RecorderOption.svelte";
+</script>
+<section class="flex w-[60%] py-3 justify-start items-center flex-col gap-5">
+    
     <div class="flex gap-2 flex-col w-full">
-        <div class="w-full aspect-[4/3] bg-white rounded"></div>
-        <div class="h-[165px] aspect-[4/3] bg-white rounded"></div>
-        <div class="h-[165px] aspect-[4/3] bg-white rounded"></div>
-        <div class="h-[165px] aspect-[4/3] bg-white rounded"></div>
+        <RecorderOption title="Screen"></RecorderOption>
+        <RecorderOption title="Camera"></RecorderOption>
+        <RecorderOption title="System Sound"></RecorderOption>
+        <RecorderOption title="Microphone"></RecorderOption>
     </div>
-    <button class="bg-white text-purple-500 py-1 px-2 rounded w-full">Start recording</button>
+    <button class="bg-white text-purple-500 py-1 px-2 rounded w-full"
+        >Start recording</button
+    >
 </section>
