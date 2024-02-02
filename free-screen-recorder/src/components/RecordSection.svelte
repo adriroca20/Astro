8bc74d2c3c05e3b1419143fb3490c902dada4908
adriroca20 <hardhe21@gmail.com>
2024-02-02 14:47:21 +0100

    feat: added comunication to recorder component and styles for pc

--- free-screen-recorder/src/components/RecordSection.svelte
+++ free-screen-recorder/src/components/RecordSection.svelte
@@ -1,14 +1,50 @@
-<script>
+<script lang="ts">
     import RecorderOption from "./RecorderOption.svelte";
     import CameraIcon from "../icons/CameraIcon.svelte";
     import ScreenIcon from "../icons/ScreenIcon.svelte";
     import SpeakerIcon from "../icons/SpeakerIcon.svelte";
     import MicrophoneIcon from "../icons/MicrophoneIcon.svelte";
+    import CameraVideo from "../icons/CameraVideo.svelte";
+    const recordingOptions:any ={
+        "Screen":false,
+        "Camera":false,
+        "Microphone":false,
+        "System Sound":false,
+    }
+
+    function handleClick({detail} : CustomEvent ){
+        console.log(detail);
+        const title:string = detail.title
+        recordingOptions[title] = detail.checked
+    }
+     async function startRecording() {
+        console.log("Recording");
+        const media = await navigator.mediaDevices.getDisplayMedia({
+            audio: true,
+            video:{frameRate:{ideal:30}}
+        })
+        const mediaRecorder = new MediaRecorder(media, {
+            mimeType:"video/webm;codecs=vp8,opus"
+        })
+        mediaRecorder.start();
+        const [video] = media.getVideoTracks();
+        video.addEventListener("ended", ()=>{
+            mediaRecorder.stop();
+        })
+        mediaRecorder.addEventListener("dataavailable", (e)=>{
+            const link = document.createElement("a")
+            link.href = URL.createObjectURL(e.data)
+            link.download = "freeRecordedVideo.webm"
+            link.click();
+        })
+    }
+
 </script>
-<section class="flex w-[60%] py-3 justify-start items-center flex-col gap-5">
+<section class="flex w-[60%] min-h-screen py-3 justify-start items-center flex-col gap-5
+sm:justify-center sm:gap-10">
     
-    <div class="flex gap-2 flex-col w-full">
-        <RecorderOption title="Screen">
+    <div class="flex gap-2 flex-col w-full sm:flex-row sm:justify-center sm:gap-4">
+        <RecorderOption title="Screen" on:toggleSelectedOption={handleClick}>
             <ScreenIcon></ScreenIcon>
         </RecorderOption>
         <RecorderOption title="Camera">
@@ -21,7 +57,14 @@
             <MicrophoneIcon></MicrophoneIcon>
         </RecorderOption>
     </div>
-    <button class="bg-white text-purple-500 py-1 px-2 rounded w-full"
-        >Start recording</button
-    >
+    <button 
+    class="bg-transparent border py-1 px-2 rounded w-full
+    flex justify-center items-center gap-3 transition-colors 
+    sm:w-1/3 sm:py-2 sm:px-4 sm:text-xl sm:text sm:font-semibold
+    hover:dark:bg-zinc-900"
+    on:click={startRecording}>
+        <span>Start recording</span>
+        <CameraVideo></CameraVideo>
+        </button>
 </section>
+
--- free-screen-recorder/src/components/RecorderOption.svelte
+++ free-screen-recorder/src/components/RecorderOption.svelte
@@ -1,13 +1,30 @@
-<script>
-    export let title;
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    export let title:string;
+    const eventDispatcher = createEventDispatcher();
+    const toggleSelectedOption = (e:MouseEvent )=>{
+        const checked:boolean = e.target.checked;
+        console.log(checked);
+        eventDispatcher("toggleSelectedOption", {title,checked});
+    }
 </script>
 
 <article
-    class="flex flex-col justify-between items-center h-full p-3 w-full aspect-[4/3] rounded-xl bg-white shadow-[0px_0px_0px_1px_rgba(9,9,11,0.07),0px_2px_2px_0px_rgba(9,9,11,0.05)] dark:bg-zinc-900 dark:shadow-[0px_0px_0px_1px_rgba(255,255,255,0.1)] dark:before:pointer-events-none dark:before:absolute dark:before:-inset-px dark:before:rounded-xl dark:before:shadow-[0px_2px_8px_0px_rgba(0,_0,_0,_0.20),_0px_1px_0px_0px_rgba(255,_255,_255,_0.06)_inset]"
+    class="flex flex-col justify-between items-center h-full p-3 w-full aspect-[4/3] relative 
+    sm:w-[20%] sm:font-semibold
+    rounded-xl bg-white shadow-[0px_0px_0px_1px_rgba(9,9,11,0.07),0px_2px_2px_0px_rgba(9,9,11,0.05)] 
+    dark:bg-zinc-900 border border-[rgba(240,240,240,0.1]
+    dark:before:pointer-events-none dark:before:absolute dark:before:-inset-px 
+    dark:before:rounded-xl
+    transition-colors 
+    hover:dark:bg-zinc-950 
+    has-[input:checked]:bg-slate-300 has-[input:checked]:text-zinc-900
+    "
 >
-    <input type="checkbox" name="selectOption" class="hidden" />
+    <input type="checkbox" name="selectOption" id="{title}" class="hidden" on:click={(e)=>toggleSelectedOption(e)}/>
+    <label for="{title}" class="absolute top-0 left-0 h-full w-full cursor-pointer"></label>
     <slot  class="flex-grow"/>
-    <label for="selectOption">{title}</label>
+    <span>{title}</span>
 </article>
 
 <style>
