d8325758f5dd0e5c4881b73e4a2d353f461aa31a
adriroca20 <hardhe21@gmail.com>
2024-02-15 23:53:20 +0100

    feat: created animation delay and timing converter

--- tailwind-animations/src/index.js
+++ tailwind-animations/src/index.js
@@ -1,11 +1,52 @@
-import createPlugin from "tailwindcss/plugin.js"
-import theme from "./theme.js"
+import createPlugin from "tailwindcss/plugin.js";
+import theme from "./theme.js";
+
 
 /**@type {import('tailwindcss/types/config').PluginCreator} */
-const pluginCreator = (api)=>{
-    console.log("plugin creator")
-}
-/**@type {import('tailwindcss/types/config').Config} */
-const pluginConfig = {theme}
+const pluginCreator = (api) => {
+  const { theme, matchUtilities, addUtilities } = api;
 
-export default createPlugin(pluginCreator, pluginConfig)
+  const dynamicUtils = {
+    'animate-delay': { css: 'animation-delay', values: theme('animationDelay') },
+    'animate-duration': { css: 'animation-duration', values: theme('animationDuration') },
+    'animate-iteration-count': { css: 'animation-iteration-count', values: theme('animationIterationCount') },
+    'animate-fill-mode': { css: 'animation-fill-mode', values: theme('animationFillMode') },
+    'animate-bezier': { css: 'animation-timing-function', values: theme('animationCubicBezier') },
+    'animate-steps': { css: 'animation-timing-function', values: theme('animationSteps'), generateValue: value => `steps(${value})` }
+  }
+
+  //Saca los valores del theme.js y los matchea con el animation-delay de CSS
+  Object.entries(dynamicUtils).forEach(([name, { css, values }]) => {
+    matchUtilities({
+      [name]: value => ({
+        [css]: value
+      })
+    }, {
+      values
+    })
+  })
+
+  addUtilities({
+    '.animate-ease': {
+      'animation-timing-function': 'ease'
+    },
+    '.animate-ease-in': {
+      'animation-timing-function': 'ease-in'
+    },
+    '.animate-ease-out': {
+      'animation-timing-function': 'ease-out'
+    },
+    '.animate-ease-in-out': {
+      'animation-timing-function': 'ease-in-out'
+    },
+    '.animate-linear': {
+      'animation-timing-function': 'linear'
+    },
+  })
+};
+
+
+/**@type {import('tailwindcss/types/config').Config} */
+const pluginConfig = { theme };
+
+export default createPlugin(pluginCreator, pluginConfig);
