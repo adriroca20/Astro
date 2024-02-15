7bd06ba6dd4124d0c2516c5fc6422ff579eae226
adriroca20 <hardhe21@gmail.com>
2024-02-15 23:52:21 +0100

    feat: added minify to css output

--- tailwind-animations/test/utils.js
+++ tailwind-animations/test/utils.js
@@ -1,22 +1,19 @@
 import tailwind from "tailwindcss"
 import postcss from "postcss"
 import animationsPlugin from "../src/index.js"
-
+import minify from "@csstools/postcss-minify"
 const TAILWIND_BASE = "@tailwind utilities;"
 export function generatePluginCSS(options = {}){
     const {inline = '',content = ''} = options
 
     return postcss([
+        minify,
         tailwind({
             plugins:[animationsPlugin],
             content:[{raw:content}]
         })
-    ]).process(`${TAILWIND_BASE} ${inline}`)
+    ]).process(`${TAILWIND_BASE} ${inline}`,{
+        from:undefined
+    })
     .then(res => res.css)
 }
-
-console.log(await generatePluginCSS(
-    {
-        content:'<div class="animate-zoom-in">Hello world</div>'
-    }
-))
