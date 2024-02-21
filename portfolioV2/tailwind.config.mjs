c013ae28774bbeb0ae67853e03403242e3b145f9
adriroca20 <hardhe21@gmail.com>
2024-02-21 14:06:15 +0100

    feat: added carousel for main projects

--- portfolioV2/tailwind.config.mjs
+++ portfolioV2/tailwind.config.mjs
@@ -1,10 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 import animated from "@adrian_rocafull/tailwind-animations"
+import flowbite from "flowbite/plugin"
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', "./node_modules/flowbite/**/*.js"],
 	darkMode:"class",
 	theme: {
 		extend: {},
 	},
-	plugins: [animated],
+	plugins: [animated, flowbite],
 }
