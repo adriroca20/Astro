dc39494bc67ce72fcb4c1817e3832f2806c33389
adriroca20 <hardhe21@gmail.com>
2024-03-04 13:14:16 +0100

    feat: portfolio configuration for vercel deployment

--- portfolioV2/astro.config.mjs
+++ portfolioV2/astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-
 export default defineConfig({
-  output: 'server',
-  adapter: vercel({
-    webAnalytics: { enabled: true }
-  }),
-  integrations: [tailwind()]
-
+  integrations: [tailwind()],
+  output: "server",
+  adapter: vercel(
+    {
+      webAnalytics: {
+        enabled: true,
+      },
+    }
+  )
 });
